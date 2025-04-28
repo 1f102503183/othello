6355d1b13f80275579c91888775551f548dd9ffb
1f102503183 <s1f102503183@iniad.org>
2025-04-28 20:56:18 +0900

    候補地ができた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,49 @@ export default function Home() {
     [1, 0],
     [1, 1],
   ];
+  const newBoard = structuredClone(board);
+  //候補地消し
+  for (let a = 0; a < 8; a++) {
+    for (let b = 0; b < 8; b++) {
+      board[a][b] %= 3;
+    }
+  }
+  //候補地探し
+  for (let a = 0; a < 8; a++) {
+    for (let b = 0; b < 8; b++) {
+      // y = a  x = b
+      if (board[a][b] === 0 || newBoard[a][b] === 3) {
+        for (let i = 0; i < directions.length; i++) {
+          const dy = directions[i][0];
+          const dx = directions[i][1];
+          if (dy + a > 7 || dy + a < 0 || dx + b > 7 || dx + b < 0) {
+            continue;
+          } else if (board[dy + a][dx + b] === 3 - turnColor) {
+            for (
+              let j = 2;
+              dy * j + a <= 7 &&
+              dy * j + a >= 0 &&
+              dx * j + b <= 7 &&
+              dx * j + b >= 0 &&
+              board[dy * j + a][dx * j + b] !== 0 &&
+              board[dy * j + a][dx * j + b] !== 3;
+              j++
+            ) {
+              if (board[dy * j + a][dx * j + b] === turnColor) {
+                newBoard[a][b] = 3;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   const clickhandler = (x: number, y: number) => {
     console.log(turnColor);
     console.log(x, y);
-    const newBoard = structuredClone(board);
-    if (newBoard[y][x] === 0) {
+    if (newBoard[y][x] === 0 || newBoard[y][x] === 3) {
       for (let i = 0; i < directions.length; i++) {
         const dy = directions[i][0];
         const dx = directions[i][1];
@@ -46,7 +84,8 @@ export default function Home() {
             dy * j + y >= 0 &&
             dx * j + x <= 7 &&
             dx * j + x >= 0 &&
-            board[dy * j + y][dx * j + x] !== 0;
+            board[dy * j + y][dx * j + x] !== 0 &&
+            board[dy * j + y][dx * j + x] !== 3;
             j++
           ) {
             console.log('enough=>for dy,dx = ', dy * j, dx * x);
@@ -68,16 +107,18 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      <div className={styles.counter} />
       <div className={styles.board}>
-        {board.map((row, y) =>
+        {newBoard.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickhandler(x, y)}>
-              {color !== 0 && (
+              {color % 3 !== 0 && (
                 <div
                   className={styles.stone}
                   style={{ background: color === 1 ? '#000' : '#fff' }}
                 />
               )}
+              {color === 3 && <div className={styles.expectPoint} />}
             </div>
           )),
         )}
