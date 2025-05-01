b10e21fe460e4ca84a93b38a78fcf2c523d6369c
1f102503183 <s1f102503183@iniad.org>
2025-05-01 14:03:34 +0900

    ルール道理枚数表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,9 +49,9 @@ export default function Home() {
             if (board[dy * j + a][dx * j + b] === turnColor) {
               if (newBoard[a][b] === 0) {
                 newBoard[a][b] = 3;
+                console.log('serch');
                 return;
               } else {
-                setTurnColor(3 - turnColor);
                 newBoard[a][b] = turnColor;
                 for (let k = j; k > 0; k--) {
                   newBoard[dy * k + a][dx * k + b] = turnColor;
@@ -65,9 +65,15 @@ export default function Home() {
     }
   };
 
+  //駒カウンター
+  const counter = (c: number) => {
+    return newBoard.flat().filter((newBoard) => newBoard === c).length;
+  };
+
   const newBoard = structuredClone(board);
 
   //候補地探し
+
   for (let a = 0; a < 8; a++) {
     for (let b = 0; b < 8; b++) {
       Serch_or_Turn(a, b);
@@ -75,18 +81,30 @@ export default function Home() {
   }
 
   const clickhandler = (x: number, y: number) => {
+    console.log([y][x]);
     Serch_or_Turn(y, x);
-    //候補地消し
-    for (let a = 0; a < 8; a++) {
-      for (let b = 0; b < 8; b++) {
-        board[a][b] %= 3;
+    if (JSON.stringify(board) !== JSON.stringify(newBoard)) {
+      //候補地消し
+      for (let a = 0; a < 8; a++) {
+        for (let b = 0; b < 8; b++) {
+          newBoard[a][b] %= 3;
+        }
       }
+      setTurnColor(3 - turnColor);
+      console.log(newBoard);
     }
     setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
-      <div className={styles.counter} />
+      <button>buck</button>
+      <div
+        className={styles.counter}
+        style={{ border: turnColor === 1 ? '3px solid #000' : '3px solid #fff' }}
+      >
+        white:{counter(2)} black:{counter(1)}
+      </div>
       <div className={styles.board}>
         {newBoard.map((row, y) =>
           row.map((color, x) => (
