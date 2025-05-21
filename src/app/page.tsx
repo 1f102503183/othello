be970067282a4d4d4adf63f1924d11309fcac436
1f102503183 <s1f102503183@iniad.org>
2025-05-22 01:36:30 +0900

    ボードの３をなくした！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import styles from './page.module.css';
-//board,newBoardへの書き込みはすべて
 
 export default function Home() {
   const [turnColor, setTurnColor] = useState(1);
@@ -16,42 +15,24 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const nextTurnColor = 3 - turnColor; //<= 次のターンカラー
-
-  //統合ボード
-  const calcBoard = structuredClone(board);
 
   const numberOfTurn = 60 - (counter(0, board) + counter(3, board)) + 1;
 
   const clickhandler = (x: number, y: number) => {
-    const newBoard = structuredClone(board);
-    //置く
-    if (board[y][x] === 0) {
-      put(newBoard, y, x, turnColor);
-    }
+    const newBoard = put(board, y, x, turnColor);
 
     if (counter(1, board) + counter(2, board) !== counter(1, newBoard) + counter(2, newBoard)) {
-      /*終了、積み、パスの処理
-      if (counter(3, newBoard) === 0) {
-        if (counter(3, newBoard) !== 0) {
-          alert(nextTurnColor === 1 ? '黒は置けないためパスします' : '白は置けないためパスします');
-        } else {
-          //終了判定
-          if (counter(0, newBoard) + counter(3, newBoard) === 0) {
-            alert(counter(1, board) > counter(2, board) ? '黒の勝ちです' : '白の勝ちです');
-          } else {
-            alert(
-              `互いに置くことができないため, ${counter(1, board) > counter(2, board) ? '黒の勝ちです' : '白の勝ちです'}`,
-            );
-          }
+      if (counter(3, putexpect(board, 3 - turnColor)) === 0) {
+        //終了、積み、パスの処理
+        if (fin(putexpect(newBoard, turnColor), turnColor)) {
+          setBoard(newBoard);
+          return;
         }
-        setBoard(newBoard);
-        return;
-      } */
-      setTurnColor(3 - turnColor);
+      } else {
+        setTurnColor(3 - turnColor);
+      }
       setBoard(newBoard);
     }
-    //ここまではレンダーされない?
   };
 
   return (
@@ -66,7 +47,7 @@ export default function Home() {
         black:{counter(1, board)} white:{counter(2, board)} {numberOfTurn}turn
       </div>
       <div className={styles.board}>
-        {calcBoard.map((row, y) =>
+        {putexpect(board, turnColor).map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickhandler(x, y)}>
               {color % 3 !== 0 && (
@@ -89,7 +70,8 @@ export default function Home() {
   );
 }
 
-const put = (newBoard: number[][], y: number, x: number, turnColor: number) => {
+const put = (board: number[][], y: number, x: number, turnColor: number): number[][] => {
+  const newBoard = structuredClone(board);
   const directions = [
     [-1, -1],
     [-1, 0],
@@ -127,6 +109,7 @@ const put = (newBoard: number[][], y: number, x: number, turnColor: number) => {
       }
     }
   }
+  return newBoard;
 };
 
 const putexpect = (board: number[][], turnColor: number): number[][] => {
@@ -144,23 +127,27 @@ const putexpect = (board: number[][], turnColor: number): number[][] => {
   //次の地点の候補地探し
   for (let a = 0; a < 8; a++) {
     for (let b = 0; b < 8; b++) {
-      for (let i = 0; i < directions.length; i++) {
-        const dy = directions[i][0];
-        const dx = directions[i][1];
-        if (dy + a > 7 || dy + a < 0 || dx + b > 7 || dx + b < 0) {
-          continue;
-        } else if (board[dy + a][dx + b] === 3 - turnColor) {
-          for (
-            let j = 2;
-            dy * j + a <= 7 &&
-            dy * j + a >= 0 &&
-            dx * j + b <= 7 &&
-            dx * j + b >= 0 &&
-            board[dy * j + a][dx * j + b] !== 0 &&
-            board[dy * j + a][dx * j + b] !== 3;
-            j++
-          ) {
-            ex[a][b] = 3;
+      if (board[a][b] === 0) {
+        for (let i = 0; i < directions.length; i++) {
+          const dy = directions[i][0];
+          const dx = directions[i][1];
+          if (dy + a > 7 || dy + a < 0 || dx + b > 7 || dx + b < 0) {
+            continue;
+          } else if (board[dy + a][dx + b] === 3 - turnColor) {
+            for (
+              let j = 2;
+              dy * j + a <= 7 &&
+              dy * j + a >= 0 &&
+              dx * j + b <= 7 &&
+              dx * j + b >= 0 &&
+              board[dy * j + a][dx * j + b] !== 0 &&
+              board[dy * j + a][dx * j + b] !== 3;
+              j++
+            ) {
+              if (board[dy * j + a][dx * j + b] === turnColor) {
+                ex[a][b] = 3;
+              }
+            }
           }
         }
       }
@@ -169,8 +156,26 @@ const putexpect = (board: number[][], turnColor: number): number[][] => {
   return ex;
 };
 
+//終了関数
+const fin = (board: number[][], turnColor: number): boolean => {
+  let result = false;
+  if (counter(3, putexpect(board, turnColor)) !== 0) {
+    alert(3 - turnColor === 1 ? '黒は置けないためパスします' : '白は置けないためパスします');
+  } else {
+    //終了判定
+    if (counter(0, board) + counter(3, board) === 0) {
+      alert(counter(1, board) > counter(2, board) ? '黒の勝ちです' : '白の勝ちです');
+    } else {
+      alert(
+        `互いに置くことができないため, ${counter(1, board) > counter(2, board) ? '黒の勝ちです' : '白の勝ちです'}`,
+      );
+    }
+    result = true;
+  }
+  return result;
+};
+
 //駒カウンター
 const counter = (c: number, board: number[][]) => {
-  console.log(board);
   return board.flat().filter((i) => i === c).length;
 };
